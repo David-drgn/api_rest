1b1ff4e7cf5b90bee322897d4f9c0277004d4f73
David <72632974+David-drgn@users.noreply.github.com>
2023-11-17 08:08:01 -0300

    -- API

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -6,63 +6,70 @@ import { AlertComponent } from '../alert/alert.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent{
-
+export class HomeComponent {
   list: Array<{
-    id: string,
-    nome: string,
-    banda: string,
-    duracao: string,
-    img: string
-  }> = []
+    id: string;
+    nome: string;
+    banda: string;
+    duracao: string;
+    img: string;
+  }> = [];
 
   listAUX: Array<{
-    id: string,
-    nome: string,
-    banda: string,
-    duracao: string,
-    img: string
-  }> = []
+    id: string;
+    nome: string;
+    banda: string;
+    duracao: string;
+    img: string;
+  }> = [];
 
-  load = false
+  load = false;
 
-  constructor(
-    private http: HttpService,
-    private dialog: MatDialog
-  ){}
+  constructor(private http: HttpService, private dialog: MatDialog) {}
 
-  ngOnInit(){
-    this.getMusic()
+  ngOnInit() {
+    this.getMusic();
   }
 
-  search(selected: any){
-    this.list = this.listAUX.filter((e:any) => e.nome.toLowerCase().includes(selected.toLowerCase()) || e.banda.toLowerCase().includes(selected.toLowerCase()) || e.duracao.toLowerCase().includes(selected.toLowerCase()))
-    if(selected == '' || selected == null) this.list = this.listAUX
+  search(selected: any) {
+    this.list = this.listAUX.filter(
+      (e: any) =>
+        e.nome.toLowerCase().includes(selected.toLowerCase()) ||
+        e.banda.toLowerCase().includes(selected.toLowerCase()) ||
+        e.duracao.toLowerCase().includes(selected.toLowerCase())
+    );
+    if (selected == '' || selected == null) this.list = this.listAUX;
   }
 
-  openDialog(title: string, message:  string){
+  openDialog(title: string, message: string) {
     this.dialog.open(AlertComponent, {
-      data:{
+      data: {
         title,
-        message
-      }
-    })
+        message,
+      },
+    });
   }
 
-  getMusic(){
-    this.load = true
-    this.http.get('getterMusic').subscribe((res: any) => {
-      if(res.erro){
-        this.load = false
-        this.openDialog('Erro', 'Erro ao puxar as músicas cadastradas')
-        return
+  getMusic() {
+    this.load = true;
+    this.http.get('getterMusic').subscribe(
+      (res: any) => {
+        if (res.erro) {
+          this.load = false;
+          this.openDialog('Erro', 'Erro ao puxar as músicas cadastradas');
+          return;
+        }
+        this.load = false;
+        this.list = res.result;
+        this.listAUX = this.list;
+      },
+      (error: any) => {
+        this.load = false;
+        this.openDialog('Erro', 'Erro ao puxar as músicas cadastradas');
+        return;
       }
-      this.load = false
-      this.list = res.result
-      this.listAUX = this.list
-    })
+    );
   }
-
 }
